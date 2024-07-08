7e8c02ac8f41a4578f5c134f37cc3027c87dedc2
Amitk-umar <135205505+Amitk-umar@users.noreply.github.com>
2024-07-08 13:22:11 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('h3').forEach(function(element) {
+        element.style.backgroundColor = "aqua";
+    });
+});
+
